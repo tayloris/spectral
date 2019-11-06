56dc3651037d1f10c919c9fa3bdb6494b10a599f
Manuel Borregales <manuelantoniobr@gmail.com>
2019-11-06 15:13:16 +0100

    taking out some lines

--- Examples/SolveProblem_1_Cheb.m
+++ Examples/SolveProblem_1_Cheb.m
@@ -3,13 +3,10 @@ function SolveProblem_1_Cheb(N)
 %p13.m - solve linear BVP     u_xx = exp(4x) , u(-1) = u(1)= 0
 warning('off','all')
 
-
  f_rhs = @(x) exp(4*x);
  u_exact = @(x) (exp(4*x)-sinh(4)*x-cosh(4))/16;
 
-
 N_max =N;
-
 
 figure('NumberTitle', 'off', 'Name', 'Chebyshev collocation method');
 
@@ -23,8 +20,8 @@ D2=D2(2:N,2:N);%remove elements related to the boundary
 f = f_rhs(x(2:N)); 
 
 condition_number(N)=cond(D2);
-u=D2\f ; %Poisson eq.solved here 
 
+u=D2\f ; %Poisson eq.solved here 
 
 u=[0;u;0]; % Imposing boundary condition
 
